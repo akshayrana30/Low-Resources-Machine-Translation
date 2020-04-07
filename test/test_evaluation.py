f6483f7c2838413ef02cd4432668da2beae33134
leohsuofnthu <leohsuofnthu@gmail.com>
2020-04-07 07:45:32 -0400

    update test evaluation

--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -105,5 +105,5 @@ lines = [s for s in lines]
 with open('../prediction.txt', 'w') as f:
     for line in lines:
         t = translate(line)
-        output = ' '.join(t[1:-1]) + '\n'
+        output = ' '.join(t[1:-1]).encode('utf-8').decode("cp950") + '\n'
         f.write(output)
