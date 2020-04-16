d90f6739584cfc079423777485372d869b04e346
leohsuofnthu <leohsuofnthu@gmail.com>
2020-04-16 06:10:42 -0400

    update'

--- generate_synthetic.py
+++ generate_synthetic.py
@@ -4,12 +4,66 @@ import logging
 from evaluator import generate_predictions
 
 
+def generate_predictions(ckpt, path_spm, input_file_path: str, pred_file_path: str):
+    """Generates predictions for the machine translation task (EN->FR).
+    You are allowed to modify this function as needed, but one again, you cannot
+    modify any other part of this file. We will be importing only this function
+    in our final evaluation script. Since you will most definitely need to import
+    modules for your code, you must import these inside the function itself.
+    Args:
+        input_file_path: the file path that contains the input data.
+        pred_file_path: the file path where to store the predictions.
+    Returns: None
+    """
+    # load input file => create test dataloader => (spm encode)
+    from data.dataloaders import prepare_test
+    BATCH_SIZE = 128
+    path_spm = path_spm
+    test_dataset, voc_size, test_max_length = prepare_test(input_file_path, path_spm, batch_size=BATCH_SIZE)
+    # create model
+    from models import mBART
+    import tensorflow as tf
+    src_vocsize = voc_size
+    tar_vocsize = voc_size
+    optimizer = tf.keras.optimizers.Adam()
+    # All the model we use is follow this setting
+    model = mBART.mBART(voc_size_src=src_vocsize,
+                        voc_size_tar=tar_vocsize,
+                        max_pe=10000,
+                        num_encoders=4,
+                        num_decoders=4,
+                        emb_size=512,
+                        num_head=8,
+                        ff_inner=1024)
+
+    # Load CheckPoint
+    ckpt_dir = ckpt
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+    status = checkpoint.restore(tf.train.latest_checkpoint(ckpt_dir))
+    status.assert_existing_objects_matched()
+
+    # Greedy Search / Beam Search and write to pred_file_path
+    import time
+    from translate import translate_batch
+    start = time.time()
+    with open(pred_file_path, 'w', encoding='utf-8') as pred_file:
+        for (batch, (inp)) in enumerate(test_dataset):
+            print("Evaluating Batch: %s" % batch)
+            translation = translate_batch(inp, batch_size=BATCH_SIZE)
+            for sentence in translation:
+                pred_file.write(sentence.strip() + '\n')
+    end = time.time()
+    print("Translation finish in %s s" % (end - start))
+
+
 def main():
     parser = argparse.ArgumentParser(
         'script to create synthetic data for backtranslation.')
     parser.add_argument('--input', help='file to be translated')
     parser.add_argument('--output', help='path to outputs - will store files here',
                         required=True)
+    parser.add_argument('--ckpt', help='file to be translated')
+    parser.add_argument('--path_spm', help='path to outputs - will store files here')
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
 
@@ -17,7 +71,7 @@ def main():
         os.makedirs(args.output)
 
     # Todo: Remember to modified the path of checkpoints in evaluator.py
-    generate_predictions(args.output, args.output)
+    generate_predictions(args.ckpt, args.path_spm,args.output, args.output)
 
 
 if __name__ == '__main__':
