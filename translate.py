11f32e8fb14ae0c1a19d88650c04883b858b1211
leohsuofnthu <leohsuofnthu@gmail.com>
2020-04-16 05:18:56 -0400

    greedy translation batch prototype

--- translate.py
+++ translate.py
@@ -3,13 +3,14 @@ from models import Transformer
 
 
 def translate_batch(model, inp, batch_size, spm, reverse=False):
-
     # if FR -> EN, reverse = True
     if reverse:
         LID = "<Fr>"
+    else:
+        LID = "<En>"
 
     batch_max_length = tf.shape(inp)[1]
-    decoder_input = tf.expand_dims([tar_tokenizer.word_index['<start> ']] * batch_size, axis=1)
+    decoder_input = tf.expand_dims([spm.piece_to_id(LID)] * batch_size, axis=1)
     output = decoder_input
 
     for i in range(batch_max_length + 50):
@@ -21,10 +22,10 @@ def translate_batch(model, inp, batch_size, spm, reverse=False):
         combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
 
         # mask for "enc_dec" multihead attention
-        dec_padding_mask = Transformer.create_padding_mask(encoder_input)
+        dec_padding_mask = Transformer.create_padding_mask(input)
 
         # predictions.shape == (batch_size, seq_len, vocab_size)
-        predictions = model(encoder_input,
+        predictions = model(input,
                             output,
                             False,
                             enc_padding_mask,
@@ -33,9 +34,9 @@ def translate_batch(model, inp, batch_size, spm, reverse=False):
 
         predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
         predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-        if (predicted_id == tar_tokenizer.word_index['<end>']).numpy().all():
+        if (predicted_id == spm.piece_to_id(LID)).numpy().all():
             return output
         output = tf.concat([output, predicted_id], axis=-1)
-    pred_sentences = tar_tokenizer.sequences_to_texts(output.numpy())
-    pred_sentences = [x.split("<end>")[0].replace("<start>", "").strip() for x in pred_sentences]
+    pred_sentences = spm.DecodeIds((output.numpy().list()))
+    pred_sentences = [x.split(LID)[0].replace(LID, "").strip() for x in pred_sentences]
     return pred_sentences
